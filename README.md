269dbcfcdc5f637f9f1aa6d995139f1b4ab27ad7
Pedro Rodrigues Chagas <pedro.chagas@aluno.ueg.br>
2025-03-26 14:06:20 -0300

    novas alterações

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Desafio DIO - Sintaxe JAVA
+# Desafio DIO
+## - Simulando Uma Conta de banco usando Java básico
 Essa é a solução do desafio proposto pela DIO.me e esse desafio faz parte do Bootcamp Java Native do Bradesco.
--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 import java.util.Locale;
-public class ContaBanco {
+public class ContaTerminal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
